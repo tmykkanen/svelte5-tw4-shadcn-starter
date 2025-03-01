f25619db9b7ca34375dd028e47efd8bb4d56d68b
tmykkanen <1270127+tmykkanen@users.noreply.github.com>
2025-03-01 13:05:45 -0600

    With Shadcn button installed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button/index.js'
+</script>
+
+<h1>Hello World!</h1>
+<Button>This is a shadcn button</Button>
